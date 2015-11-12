d168d16b9d1cabe764d3775a344954be90b9e79d
whittefang <whittefang@gmail.com>
2015-11-11 20:55:23 -0600

    InitialCommit

---
+++ MoveABox/Assets/MoveIt.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveIt : MonoBehaviour {
+	public float Speed;
+	SpriteRenderer SR;
+	Rigidbody2D RB;
+	// Use this for initialization
+	void Start () {
+	
+		SR = GetComponent<SpriteRenderer> ();
+		RB = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float XAxis = Input.GetAxis ("Horizontal");
+		float YAxis = Input.GetAxis ("Vertical");
+
+
+		RB.velocity = new Vector3 (XAxis * Speed, YAxis * Speed, 0);
+		SR.color = new Color (Mathf.Abs (XAxis), 0, Mathf.Abs (YAxis), 1);
+			
+		if (Input.GetKey(KeyCode.Escape)){
+			Application.Quit();
+		}
+	}
+}
